bf565cf303fc7db763a03d0bd8a73473250a7819
hx <1014231151@qq.com>
2021-09-09 15:38:06 +0800

    feat: 新增git clone: clone的shell脚本,clone的Dockerfile,以及dsl

---
+++ script/clone.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+set -e
+
+GIT_PROJECT=`basename "${JIANMU_REMOTE_URL}" .git`
+TAGBRANCH=""
+
+if [[ -z "${JIANMU_NETRC_MACHINE}" ]]; then
+  echo "[ERROR] The git machine configuration is missing"
+  exit 1
+fi
+
+if [[ -z "${JIANMU_REMOTE_URL}" ]]
+then
+  echo "[ERROR] The necessary git source configuration is missing"
+  exit 1
+fi
+
+if [[ -n "${JIANMU_NETRC_USERNAME}" && -n "${JIANMU_NETRC_PASSWORD}" ]]
+then
+  mkdir -p ${HOME}
+  	cat <<EOF > ${HOME}/.netrc
+  machine ${JIANMU_NETRC_MACHINE}
+  login ${JIANMU_NETRC_USERNAME}
+  password ${JIANMU_NETRC_PASSWORD}
+EOF
+  chmod 600 ${HOME}/.netrc
+else
+  echo "[WARN] The username or password configuration is missing,try use ssh"
+fi
+
+
+if [[ -n "${JIANMU_SSH_KEY}" ]]; then
+	mkdir -p ${HOME}/.ssh
+	echo -n "$JIANMU_SSH_KEY" > ${HOME}/.ssh/id_rsa
+	chmod 600 ${HOME}/.ssh/id_rsa
+
+	touch ${HOME}/.ssh/known_hosts
+	chmod 600 ${HOME}/.ssh/known_hosts
+	ssh-keyscan -H ${JIANMU_NETRC_MACHINE} > ${HOME}/.ssh/known_hosts 2> /dev/null
+else
+  echo "[WARN] The SSH configuration is missing,try use username,password"
+fi
+
+if [ ! -d ${JIANMU_SHARE_DIR} ]; then
+    mkdir -p ${JIANMU_SHARE_DIR}
+fi
+cd ${JIANMU_SHARE_DIR}
+git clone ${JIANMU_REMOTE_URL}
+cd "${GIT_PROJECT}"
+
+#refs/heads/master
+#refs/tags/1.0.0
+#if ${JIANMU_REF} is tag（refs/tags/1.0.0） checkout
+#if ${JIANMU_REF} is branch（refs/heads/master）checkout branch name
+echo ${JIANMU_REF} > ref
+CHECKOUT_REF=`cut ref -d "/" -f 3`
+CHECKOUT_TYPE=`cut ref -d "/" -f 2`
+
+if [[ ${CHECKOUT_TYPE} == "heads" ]]; then
+  git checkout ${CHECKOUT_REF}
+  TAGBRANCH="git_branch"
+  echo "current branch:"
+else
+  git checkout ${JIANMU_REF}
+  TAGBRANCH="git_tag"
+  echo "current tags:"
+fi
+# echo current branch or tag
+git branch
+
+echo "resultFile:"
+mkdir -p /usr/${GIT_PROJECT}
+echo -e "{\n"\"git_path\"" ":" "\"${JIANMU_SHARE_DIR}/${GIT_PROJECT}\""","\n"\"${TAGBRANCH}\"" ":" "\"${CHECKOUT_REF}\""\n"}"" > resultFile
+mv resultFile /usr/${GIT_PROJECT}
+
+cat /usr/${GIT_PROJECT}/resultFile
