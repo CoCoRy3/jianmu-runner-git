beab3df1b6418fd38047ec123ed2f93922db0670
hx <1014231151@qq.com>
2021-12-01 14:28:44 +0800

    feat: 增加对非22端口的判断

--- script/clone.sh
+++ script/clone.sh
@@ -37,12 +37,15 @@ if [[ -n "${JIANMU_SSH_KEY}" ]]; then
 
  # compatible with non-22 ports
   RESULT=`echo ${JIANMU_NETRC_MACHINE} | grep ":"`
+  echo "判断"
   if [[ ${RESULT} != "" ]];then
+    echo "!="
     echo ${JIANMU_NETRC_MACHINE} > /tmp/machine
     PORT=`cut /tmp/machine -d ":" -f 2`
     IP=`cut /tmp/machine -d ":" -f 1`
     ssh-keyscan -H -p ${PORT} ${IP} > ${HOME}/.ssh/known_hosts 2> /dev/null
-  elif [ ${RESULT} == "" ]; then
+  else
+    echo "=="
 	  ssh-keyscan -H ${JIANMU_NETRC_MACHINE} > ${HOME}/.ssh/known_hosts 2> /dev/null
   fi
 else
