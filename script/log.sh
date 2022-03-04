bdfc95ae15a0b5121cabe4b61abc5ee590d38871
hx <1014231151>
2022-03-04 10:20:41 +0800

    release: git_log:1.0.0

--- script/log.sh
+++ script/log.sh
@@ -5,4 +5,4 @@ cd "${JIANMU_GIT_PATH}"
 # 1. Extract log
 git log --pretty=format:"%H 「……」 %an 「……」 %ae 「……」 %cn 「……」 %ce 「……」 %cd 「……」 %s" > log
 # 2. Format and generate output using Python
-python log2json.py
+python /usr/local/bin/log2json.py
